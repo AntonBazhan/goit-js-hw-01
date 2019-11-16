a6a78f604820036ac2a7fd2e409bec0d07745cef
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2019-11-16 03:26:29 +0200

    1

---
+++ task-1.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const name = 'Генератор защитного поля';
+
+let prise;
+
+let sameMessage;
+
+prise= 1000;
+
+sameMessage = `Выбран «${name}», цена за штуку ${prise} кредитов`;
+
+console.log(sameMessage);
+
+prise = 2000;
+
+sameMessage = `Выбран «${name}», цена за штуку ${prise} кредитов`;
+
+console.log(sameMessage);
