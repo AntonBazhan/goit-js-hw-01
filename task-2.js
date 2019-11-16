516f58cf7828ec9531639aad4f2c7a475945e417
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2019-11-16 23:06:57 +0200

    2

---
+++ task-2.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const total = 100;
+
+let ordered;
+
+ordered = 50;
+
+let totalProduct = total < ordered ? '"На складе недостаточно твоаров!"' : '"Заказ оформлен, с вами свяжется менеджер"';
+
+console.log(totalProduct)
+
+ordered = 20;
+
+totalProduct = total < ordered ? '"На складе недостаточно твоаров!"' : '"Заказ оформлен, с вами свяжется менеджер"';
+
+console.log(totalProduct)
+
+ordered = 80;
+
+totalProduct = total < ordered ? '"На складе недостаточно твоаров!"' : '"Заказ оформлен, с вами свяжется менеджер"';
+
+console.log(totalProduct)
+
+ordered = 1300;
+
+totalProduct = total < ordered ? '"На складе недостаточно твоаров!"' : '"Заказ оформлен, с вами свяжется менеджер"';
+
+console.log(totalProduct)
+
+
+
+
+
